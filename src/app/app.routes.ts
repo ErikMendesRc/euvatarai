c017db708ec2549eb6e1d4c300db73a460f027de
Euvatar <159192198+Euvatar@users.noreply.github.com>
2024-05-14 11:05:06 -0300

    ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { TeletransporteComponent } from './teletransporte/teletransporte.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }, // Rota padrão para a HomeComponent
+    { path: '', component: HomeComponent },
     { path: 'teletransporte', component: TeletransporteComponent },
-    // Outras rotas, se houver...
   ];
